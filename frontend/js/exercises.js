555a146cd7d19008240a3b78e0e502bd6162fed2
Julian Alves <julianalvesinstinto@gmail.com>
2025-05-07 19:36:24 -0300

    feat: added classes and exercises

---
+++ frontend/js/exercises.js
@@ -0,0 +1,25 @@
+//altera  os painéis ao colicar na seta
+const botoes = document.querySelectorAll ('.botao');
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const painel = botao.closest('.accordion').querySelector('.painel');
+        const seta = botao.querySelector('.seta');
+
+        const estaAberto = painel.style.display === 'block';
+        painel.style.display = estaAberto ? 'none' : 'block';
+        
+        seta.classList.toggle('girada', !estaAberto);
+        //gira a seta se quiser (exemplo: seta.classList.toggle('girada'))
+    });
+});
+
+//Menu ativo ao clicar
+const menuLinks = document.querySelectorAll('.menu ul li a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        menuLinks.forEach(l => l.classList.remove('ativo'));
+        this.classList.add('ativo');
+    });
+});
